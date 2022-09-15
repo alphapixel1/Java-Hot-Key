055c82160f21c80172469adc7a383dc86d69ba4f
alphapixel1 <alphapixel2@gmail.com>
2022-09-15 02:03:48 -0400

    swing ui sucks

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import UI.MainForm;
 import UI.MainGUI;
 import com.github.kwhat.jnativehook.*;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("lol");
-        MainGUI.Show();
-        var mf=new MainForm();
+    //    MainGUI.Show();
 
+        var mf2=new MainForm();
+        mf2.show();
     }
+
 }
--- src/UI/MainForm.java
+++ src/UI/MainForm.java
@@ -1,8 +1,88 @@
 package UI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class MainForm {
-    private JEditorPane editorPane1;
-    private JButton button1;
+public class MainForm extends JFrame {
+    private JPanel mainPanel;
+    private JTextPane textPane1;
+    //private JList list1;
+    private JButton deleteButton;
+    private JButton editButton;
+    private JButton compileButton;
+
+    public MainForm(){
+        this.setSize(500,500);
+        this.setContentPane(mainPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+        Init Menu
+         */
+        var menu = new MenuBar();
+        this.setMenuBar(menu);
+
+        /*
+        File Menu
+         */
+        var file=new Menu("File");
+        menu.add(file);
+
+        var open =new MenuItem("Open");
+        open.setActionCommand("fuck");
+        open.setShortcut(new MenuShortcut(KeyEvent.VK_O));
+
+        file.add(open);
+
+        var _new =new MenuItem("New");
+        _new.setShortcut(new MenuShortcut(KeyEvent.VK_N));
+        file.add(_new);
+
+        var save =new MenuItem("Save");
+        save.setShortcut(new MenuShortcut(KeyEvent.VK_S));
+        file.add(save);
+
+        var export =new MenuItem("Export");
+        file.add(export);
+
+        /*
+        Edit Menu
+        */
+        var edit=new Menu("Edit");
+        menu.add(edit);
+
+        var clear=new MenuItem("Clear Keybinds");
+        edit.add(clear);
+
+        var reset=new MenuItem("Reset LUA");
+        edit.add(reset);
+
+        /*
+        Tool Menu
+         */
+        var tool=new Menu("Tools");
+        menu.add(tool);
+
+        var log=new MenuItem("Ouput Log");
+        tool.add(log);
+
+        var keyRef=new MenuItem("Key Reference");
+        tool.add(keyRef);
+
+        var quick=new MenuItem("Quick Keybind");
+        tool.add(quick);
+
+        /*
+        Quick Keybind Helper
+         */
+        var quickHelper=new Menu("Press any key");
+        menu.add(quickHelper);
+     //   list1.setListData(new Object[]{"one","two","three"});
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
