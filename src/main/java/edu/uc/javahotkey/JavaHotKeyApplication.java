2b7e75da871efc26be6401c3772b281832770ca1
mchughjk <mchughjk@mail.uc.edu>
2022-09-20 10:28:44 -0400

    Spring project init

---
+++ src/main/java/edu/uc/javahotkey/JavaHotKeyApplication.java
@@ -0,0 +1,13 @@
+package edu.uc.javahotkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHotKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHotKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/uc/javahotkey/JavaHotKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uc.javahotkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHotKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
