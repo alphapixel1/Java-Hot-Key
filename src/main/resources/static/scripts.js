793ef37b9014589b725bb8ecbba01f7f5f4d4494
Brendan Payne <payneb2@mail.uc.edu>
2022-10-02 19:42:25 -0400

    Button Text Changes Depending on Dark Mode State

---
+++ src/main/resources/static/scripts.js
@@ -0,0 +1,10 @@
+function lightModeToggle() {
+    const element = document.body;
+    element.classList.toggle("light-mode");
+    const button = document.getElementsByClassName("button6")[0];
+    if (element.classList.contains("light-mode")) {
+        button.innerHTML = "Dark Mode";
+    } else {
+        button.innerHTML = "Light Mode";
+    }
+}
