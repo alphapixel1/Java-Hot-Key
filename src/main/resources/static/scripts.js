3642b4e3358578099105d44d42cd02f86969140b
Andrew Glanz <andrewvita0@gmail.com>
2022-10-22 03:38:42 -0400

    implemented a modal window popup function for view keybinds. needs thymeleaf implementation.

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -10,12 +10,9 @@ function lightModeToggle() {
 }
 
 function modalWindow() {
-// Get the modal
-    var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    const modal = document.getElementById("myModal");
+    const btn = document.getElementById("myBtn");
+    const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
     btn.onclick = function () {
