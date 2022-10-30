249350c3fd1b08edaba57a90cf844ed8c5a8031c
CM2inc <cepadam.11@gmail.com>
2022-10-30 17:06:39 -0400

    Formatted method indentation the correct way according to https://rhamedy.medium.com/a-short-summary-of-java-coding-best-practices-31283d0167d3

--- src/main/java/edu/uc/javahotkey/JavaHotKeyApplication.java
+++ src/main/java/edu/uc/javahotkey/JavaHotKeyApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class JavaHotKeyApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         SpringApplication.run(JavaHotKeyApplication.class, args);
     }
 
