866ca9375b9064f6f0ef4d6cdbba088e6ca111b6
Gabriel Phan <phanga@mail.uc.edu>
2022-10-30 20:55:17 -0400

    Adding the RetrofitClientInstance

---
+++ src/main/java/edu/uc/javahotkey/dao/RetrofitClientInstance.java
@@ -0,0 +1,20 @@
+package edu.uc.javahotkey.dao;
+
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Retrofit;
+
+public class RetrofitClientInstance {
+
+    private static Retrofit retrofit;
+
+
+    public static Retrofit getRetrofitInstance() {
+        if(retrofit == null){
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseURL(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
