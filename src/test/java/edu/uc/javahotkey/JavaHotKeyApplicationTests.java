86de2a08b909135de4222aa6d8b17fb727730a38
alphapixel1 <alphapixel2@gmail.com>
2022-11-17 17:28:04 -0500

    added felix's unit tests

--- src/test/java/edu/uc/javahotkey/JavaHotKeyApplicationTests.java
+++ src/test/java/edu/uc/javahotkey/JavaHotKeyApplicationTests.java
@@ -1,7 +1,10 @@
 package edu.uc.javahotkey;
 
+import edu.uc.javahotkey.dto.Project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JavaHotKeyApplicationTests {
@@ -10,6 +13,21 @@ class JavaHotKeyApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void verifyProjectID(){
+        int id = 2;
+        Project project = new Project(2,"");
+        project.setId(id);
+        assertEquals(id,project.getId());
+    }
 
+
+    @Test
+    void verifyProjectName(){
+        String name = "random";
+        Project project = new Project(0,"random2");
+        project.setName(name);
+        assertEquals(name,project.getName());
+    }
 
 }
