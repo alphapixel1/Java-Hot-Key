c82b922364750b8616355bc48255cdc0e7f94474
alphapixel1 <alphapixel2@gmail.com>
2022-11-30 22:25:32 -0500

    added tests

--- src/test/java/edu/uc/javahotkey/JavaHotKeyApplicationTests.java
+++ src/test/java/edu/uc/javahotkey/JavaHotKeyApplicationTests.java
@@ -1,10 +1,15 @@
 package edu.uc.javahotkey;
 
+import edu.uc.javahotkey.dto.KeyMap;
 import edu.uc.javahotkey.dto.Project;
+import edu.uc.javahotkey.lua.CompilerMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class JavaHotKeyApplicationTests {
@@ -28,6 +33,27 @@ class JavaHotKeyApplicationTests {
         Project project = new Project(0,"random2");
         project.setName(name);
         assertEquals(name,project.getName());
+    }
+    @Test
+    void verifyCompilerMessage(){
+        String message ="test";
+        CompilerMessage cm=new CompilerMessage(message,true);
+        assertEquals(cm.Message, "test");
+    }
+
+    @Test
+    void verifyCompilerMessageIsError(){
+        String message ="test";
+        CompilerMessage cm=new CompilerMessage(message,true);
+        assertTrue(cm.isError);
+    }
+
+    @Test
+    void verifyKeyMap(){
+            var keyMap=new KeyMap();
+            keyMap.functionName="hi";
+            keyMap.keymap=new ArrayList<>();
+            assertEquals(keyMap.toString(),"{ function=hi, keymap=[]}");
     }
 
 }
